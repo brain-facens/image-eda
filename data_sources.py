2c7fe6ef7a29839528ef8b97fa6ab052a030dc3d
Luisa <lu_rfa@hotmail.com>
2020-09-25 15:18:46 -0300

    removes if and else for lists

--- data_sources.py
+++ data_sources.py
@@ -51,13 +51,11 @@ class LocalCsvSource(DataSource):
         self.file_path = file_path if isinstance(file_path, list) else [file_path]
         self.image_path = image_path if isinstance(image_path, list) else [image_path]
 
-        if isinstance(file_path, list):
-            for file in self.file_path:
-                self.data = self.data.append(pd.read_csv(file))
-            self.data["dataset_name"] = "dataset_name"
-        else:
-            self.data = pd.read_csv(self.file_path)
-
+        
+        for file in self.file_path:
+            self.data = self.data.append(pd.read_csv(file))
+        
+        self.data["dataset_name"] = "dataset_name"
         #self.data = [pd.read_csv(self.file_path) for path in self.file_path]
 
     def count(self):
