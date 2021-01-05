9162372ab5d15bb11aaf1f69c3578a90c45b569c
Luisa <lu_rfa@hotmail.com>
2021-01-05 13:49:15 -0300

    Fixed split logic of arguments

--- mlflow_init.py
+++ mlflow_init.py
@@ -9,7 +9,7 @@ def main():
     global args
     parser = argparse.ArgumentParser(description="Main script for ImageEDA")
     parser.add_argument("--experiment_name", type=str, help="Experiment's name")
-    parser.add_argument("--dataset_name", type=str, help="Dataset's name")
+    parser.add_argument("--dataset_name", type=str, help="Dataset's identification")
     parser.add_argument("--image_path", type=str, help="Absolute image's path")
     parser.add_argument("--annotation_path", type=str, help="Absolute annotation's path")
     parser.add_argument("--model_name", type=str, help="Model name. ie: vgg16")
@@ -21,10 +21,10 @@ def main():
     args.dataset_name
     experiment_id = os.getenv("MLFLOW_EXPERIMENT_ID")
 
-    args.dataset_name.split()
-    args.image_path.split()    
-    args.annotation_path.split()
-    args.dr_method.split()
+    dataset_name = args.dataset_name.split()
+    image_path = args.image_path.split()    
+    annotation_path = args.annotation_path.split()
+    dr_method = args.dr_method.split()
 
     with mlflow.start_run(experiment_id=experiment_id) as curr_run:
         
@@ -33,17 +33,18 @@ def main():
 
         image_eda = []
 
-        for i in range(len(args.dataset_name)):
+        for i in range(len(dataset_name)):
             image_eda.append(ImageEDA(experiment_name=args.experiment_name, 
-            data_source=LocalCsvSource(args.annotation_path[i],args.image_path[i], args.dataset_name[i]), 
-            dr_method=args.dr_method[i], batch_size=args.batch_size, n_components=args.n_components))
+            data_source=LocalCsvSource(annotation_path[i],image_path[i], dataset_name[i]), 
+            dr_method=dr_method[i], batch_size=args.batch_size, n_components=args.n_components))
+            image_eda[i].predict_feature_map()
+            image_eda[i].partial_fit()
+            image_eda[i].transform()
+            image_eda[i].visualize('classes_config.txt')
 
 
 
-        image_eda.predict_feature_map()
-        image_eda.partial_fit()
-        image_eda.transform()
-        image_eda.visualize('classes_config.txt')
+        
 
 if __name__ == "__main__":
     main()
